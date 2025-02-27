85849de6cc1c634bc2cb52ab84aad34bafc23da4
lgopiset <lgopiset@gmail.com>
2025-02-27 17:41:17 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,4 +1,27 @@
-public class Test {
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
+public class GetUniqueRecords {
 
+    public static List<String> getUniqueRecords(List<String> strings) {
+        // Use a HashSet to remove duplicates
+        Set<String> uniqueStrings = new HashSet<>(strings);
+        // Convert the set back to a list
+        return new ArrayList<>(uniqueStrings);
+    }
+
+    public static void main(String[] args) {
+        List<String> strings = new ArrayList<>();
+        strings.add("apple");
+        strings.add("banana");
+        strings.add("apple");
+        strings.add("orange");
+        strings.add("banana");
+        strings.add("pear");
+
+        List<String> uniqueRecords = getUniqueRecords(strings);
+        System.out.println("Unique records: " + uniqueRecords);
+    }
 }
